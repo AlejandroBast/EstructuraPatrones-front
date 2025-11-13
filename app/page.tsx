6b758a22596d9823c1d8c5670facaa84f52d3c95
AlejandroBast <nicohlas.personal@gmail.cm>
2025-11-13 00:40:31 -0500

    Reinicio del repo en 0

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Frontend - Personal Finance Helper</h1>
-    </div>
-  )
-}
