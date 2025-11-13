d38e6194553b1b3616e1c603361abb5533d2cb8d
miikeepp <michael.lagos@campusucc.edu.co>
2025-11-13 15:11:17 -0500

    Agregar logo

--- .next/dev/server/chunks/ssr/Desktop_Frontenn Patrones de sftwr_EstructuraPatrones-front_app_795c2a3b._.js
+++ .next/dev/server/chunks/ssr/Desktop_Frontenn Patrones de sftwr_EstructuraPatrones-front_app_795c2a3b._.js
@@ -1,7 +1,7 @@
 module.exports = [
 "[project]/Desktop/Frontenn Patrones de sftwr/EstructuraPatrones-front/app/favicon.ico (static in ecmascript, tag client)", ((__turbopack_context__) => {
 
-__turbopack_context__.v("/_next/static/media/favicon.0b3bf435.ico");}),
+__turbopack_context__.v("/_next/static/media/favicon.5c5a783a.ico");}),
 "[project]/Desktop/Frontenn Patrones de sftwr/EstructuraPatrones-front/app/favicon.ico.mjs { IMAGE => \"[project]/Desktop/Frontenn Patrones de sftwr/EstructuraPatrones-front/app/favicon.ico (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript)", ((__turbopack_context__) => {
 "use strict";
 
@@ -13,8 +13,8 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$Frontenn__Patrone
 ;
 const __TURBOPACK__default__export__ = {
     src: __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$Frontenn__Patrones__de__sftwr$2f$EstructuraPatrones$2d$front$2f$app$2f$favicon$2e$ico__$28$static__in__ecmascript$2c$__tag__client$29$__["default"],
-    width: 256,
-    height: 256
+    width: 100,
+    height: 100
 };
 }),
 ];
